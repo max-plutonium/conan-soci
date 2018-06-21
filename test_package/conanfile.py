9f0017a2c9fe56862e2862116561d730c7cecb16
v.krapivin <v.krapivin@avantize.com>
2018-06-21 11:49:54 +0300

    Fixed test package building.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,24 +2,20 @@ import os
 from conans import ConanFile, CMake, tools, RunEnvironment
 
 class SociTestConan(ConanFile):
-    user = os.getenv("CONAN_USERNAME", "akazantsev")
-    channel = os.getenv("CONAN_CHANNEL", "testing")
-    requires = "SOCI/4.0.0@{}/{}".format(user, channel), \
-               "catch/1.5.0@TyRoXx/stable"
+    requires = "catch/1.5.0@TyRoXx/stable"
     settings = "os", "compiler", "arch", "build_type"
-    default_options = "SOCI:with_postgresql=True"
     generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure()
         cmake.build()
 
     def imports(self):
-        self.copy(pattern="*.dll", dst="bin", src="bin")
-        self.copy(pattern="*.dylib", dst="bin", src="lib")
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='bin', src='lib')
 
     def test(self):
-        env = RunEnvironment(self)
-        with tools.environment_append(env.vars):
+        if not tools.cross_building(self.settings):
             self.run(os.path.join("bin", "test-empty dummy"))
